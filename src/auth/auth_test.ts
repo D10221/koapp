845945ac2b03ea385592d24fba5877e7c39b5c7a
Daniel <danieliozzi@gmail.com>
2016-06-21 08:51:05 +0200

    auth:200/401

--- src/auth/auth_test.ts
+++ src/auth/auth_test.ts
@@ -3,7 +3,33 @@ import * as Koa from 'koa';
 import * as st from 'supertest';
 
 describe('auth', ()=> {
-    it('works',async ()=>{
+    
+    it('200',async ()=>{
+        let app = new Koa();
+        let getUser = (name,pass)=> { return { name: name , password: pass } };
+        app.use(auth.auth(getUser));        
+        app.use((ctx,next)=>{
+            ctx.body = (ctx as any).user;
+        })
+        let request = st(app.listen());
+        await new Promise((resolve,reject)=>{
+            request
+            .get('/')
+            .set('Authentication', `Basic ${new Buffer('admin:admin').toString('Base64')}`)
+            .expect(200)            
+            .expect({name: 'admin', password: 'admin'})
+            .end((e,r)=>{
+                if(e){
+                    reject(e);
+                    return ;
+                }
+                resolve(r);
+            });
+
+        })
+    })
+
+    it('401',async ()=>{
         let app = new Koa();
         app.use(auth.auth((name,pass)=> {            
             return {
@@ -17,9 +43,9 @@ describe('auth', ()=> {
         await new Promise((resolve,reject)=>{
             request
             .get('/')
-            .set('Authentication', `Basic ${new Buffer('admin:admin').toString('Base64')}`)
-            .expect(200)            
-            .expect({name: 'admin', password: 'admin'})
+            //.set('Authentication', `Basic ${new Buffer('admin:admin').toString('Base64')}`)
+            .expect(401)            
+            //.expect({name: 'admin', password: 'admin'})
             .end((e,r)=>{
                 if(e){
                     reject(e);
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -10,16 +10,20 @@ export function auth(getUser: (name:string, pass:string)=> any ) : Middleware {
     let regex = /Basic\s+(.*)/i;    
     
     return async function(ctx, next)  {                
+        
         let r =  regex.exec(ctx.headers['authentication']);
-        if(!r) throw(401);
+        if(!r) ctx.throw(401);
 
         let auth =   new Buffer(r[1], 'base64').toString();    
-        if(!auth) throw(401);
+        if(!auth) ctx.throw(401);
         
         let parts = /^([^:]*):(.*)$/.exec(auth);
-        if(auth){                                    
-            (ctx as any).user = getUser(parts[1], parts[2])
-        }
+                             
+        let user = getUser(parts[1], parts[2]);
+        if(!user) ctx.throw(401);
+
+        //ctx.app as App        
+        (ctx as any).user = user;        
         next();
     }
 }
